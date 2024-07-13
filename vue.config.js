972030dc82a9b27854e2633ae3860c167900c4ce
chiba008007 <chiba00807@gmail.com>
2024-07-13 23:02:42 +0900

    first

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "./",
+  assetsDir: "",
+  outputDir: "docs",
+
+  devServer: {
+    host: "localhost",
+  },
+
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
 });
