4638153016ea86dc645e6aa219bc4b6dacffce85
chiba008007 <chiba00807@gmail.com>
2024-07-28 22:51:32 +0900

    ログイン

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,65 +1,37 @@
-<script lang="ts">
-export default {
-  data: () => ({
-    form: false,
-    email: null,
-    password: null,
-    loading: false,
-  }),
-
-  methods: {
-    onSubmit() {
-      if (!this.form) return;
-
-      this.loading = true;
-
-      setTimeout(() => (this.loading = false), 2000);
-    },
-    // required(v) {
-    //   return !!v || "Field is required";
-    // },
-  },
-};
-</script>
+<script lang="ts"></script>
 <template>
   <v-container>
-    <v-card class="mx-auto px-6 py-8" variant="plain">
-      <v-form v-model="form" @submit.prevent="onSubmit">
-        <v-text-field
-          v-model="email"
-          :readonly="loading"
-          :rules="[required]"
-          class="mb-2"
-          label="Email"
-          clearable
-        ></v-text-field>
-
-        <v-text-field
-          v-model="password"
-          :readonly="loading"
-          :rules="[required]"
-          label="Password"
-          placeholder="Enter your password"
-          clearable
-        ></v-text-field>
-
-        <br />
-
-        <v-btn
-          :disabled="!form"
-          :loading="loading"
-          color="success"
-          size="large"
-          type="submit"
-          variant="elevated"
-          block
-        >
-          ログイン
-        </v-btn>
-      </v-form>
-      <v-btn link nuxt class="mt-4 w-100" variant="text">
-        パスワード忘れた人
-      </v-btn>
-    </v-card>
+    <v-row>
+      <v-col cols="12">
+        <v-textarea
+          label="メールアドレス"
+          rows="1"
+          variant="outlined"
+          auto-grow
+          hide-details="false"
+        ></v-textarea>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-textarea
+          label="パスワード"
+          rows="1"
+          variant="outlined"
+          auto-grow
+          hide-details="false"
+        ></v-textarea>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-btn class="w-100" color="primary">ログイン</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-btn class="w-100" color="default">パスワード忘れた人</v-btn>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
